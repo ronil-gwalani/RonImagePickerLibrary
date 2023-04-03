e5e292175e944cb5aa834da4323c5ce554d2bce3
Ronil <110444453+ronil-gwalani@users.noreply.github.com>
2023-04-03 16:26:40 +0530

    Update ImagePickerMainActivity.kt

--- RonImagePicker/src/main/java/com/ronil/ronimagepicker/ImagePickerMainActivity.kt
+++ RonImagePicker/src/main/java/com/ronil/ronimagepicker/ImagePickerMainActivity.kt
@@ -162,7 +162,7 @@ class ImagePickerMainActivity : AppCompatActivity() {
     private fun openCamera() {
         val file = createImageFile()
         this.cameraImageUri =
-            FileProvider.getUriForFile(this, "com.ronil.ronimagepicker.fileprovider", file)
+            FileProvider.getUriForFile(this, "com.ronil.imagepicker.fileprovider", file)
         cameraLauncher.launch(this.cameraImageUri)
 
     }
